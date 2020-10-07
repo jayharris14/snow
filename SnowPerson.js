55ef6ee6e17c91148b4cd0982964045cdf2a86b4
jayharris14 <70274419+jayharris14@users.noreply.github.com>
2020-10-07 15:11:58 -0400

    Add files via upload

---
+++ SnowPerson.js
@@ -0,0 +1,27 @@
+
+  d3.select("#graph")
+  .append("circle")
+  .attr("cx", 100)
+  .attr("cy", 100)
+   .attr("r",50)
+   .attr("fill", "white")
+   .attr("stroke", "black")
+
+  d3.select("#graph")
+  .append("circle")
+  .attr("cx", 80)
+  .attr("cy", 90)
+   .attr("r",3)
+   .attr("fill", "white")
+   .attr("stroke", "black")
+
+  d3.select("#graph")
+  .append("circle")
+  .attr("cx", 120)
+  .attr("cy", 90)
+   .attr("r",3)
+   .attr("fill", "white")
+   .attr("stroke", "black")
+
+
+
